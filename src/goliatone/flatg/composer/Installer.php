dad649fc4d8b839abf67e6332d90ab1d3c065903
goliatone <burgosemi@gmail.com>
2013-08-01 18:26:04 -0400

    Update composer post install script.

--- src/goliatone/flatg/composer/Installer.php
+++ src/goliatone/flatg/composer/Installer.php
@@ -1,12 +1,14 @@
 <?php
 namespace goliatone\flatg\composer;
 
+use Composer\Script\Event;
+
 class Installer
 {
-    public static function build($e)
+    public static function build(Event $event)
     {
         echo "Installing flatG";
-        print "Installing FlatG";
-        error_log("Installing FlatG!");
+        exec("touch IMHERE.md");
+        
     }
 }
