f7f785acbd876ed74ffe64bd497abdd9648f5769
goliatone <burgosemi@gmail.com>
2013-08-14 17:56:29 -0400

    Installer progress.

--- installer/installer.sh
+++ installer/installer.sh
@@ -3,7 +3,8 @@
 # bash <(curl -s https://raw.github.com/goliatone/flatg-core/master/installer/install.sh)
 ###############################
 FLATG_RELEASE=v0.0.0
-
+ROOT='./'
+THEME_URL=
 echo "Downloading composer.json file..."
 COMPOSER_URL="https://raw.github.com/goliatone/flatg-core/master/installer/composer.json" 
 curl -L -O $COMPOSER_URL
@@ -13,3 +14,13 @@ composer install
 
 echo "Creating flatg dir structure at $ROOT..."
 mkdir -p ${ROOT}/{assets,articles,themes}
+
+THEME_RELEASE=master
+THEME="https://github.com/goliatone/flatg-core/archive/${THEME_RELEASE}.tar.gz" 
+
+echo "Downloading flatg theme..."
+popd ./themes
+curl -L -O $THEME
+# github will redirect this link, you end up having name missmatch
+THEME_DIR=$(tar -ztf "${FLATG_RELEASE}.tar.gz" | head -n 1)
+tar -zxvf "${FLATG_RELEASE}.tar.gz"
