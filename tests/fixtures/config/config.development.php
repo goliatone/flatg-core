66fbbef0718d8324dc62cd8b62b2c4d1730098b6
goliatone <burgosemi@gmail.com>
2013-12-30 00:18:36 -0500

    Test: Updated fixture config development.

--- tests/fixtures/config/config.development.php
+++ tests/fixtures/config/config.development.php
@@ -1,57 +1,9 @@
 <?php
-$passwords = array(
-    'dropbox'=>array(
-        'key'=>'xxxxxxxxxxxxx',
-        'secret'=>'xxxxxxxxxxxxx',
-        
-    ),
-    'github'=>array(
-        'key'=>'xxxxxxxxxxxxx',
-        'secret'=>'xxxxxxxxxxxxx',
-    ),
-);
 
-//TODO: Normalize basePath & base_url. Make robust.
-
-//TODO: We need to move index from router, and then 
-//handle/simplify all path management!! look into an 
-//autoloader. Also, could we manage this in a helper?!
 return array(
-    'base_path' => APPPATH,
-    'view_dir'  => "themes",
-    'theme' => 'gone',
-    'articles_extension' =>'yaml',
-    'articles_path' => ROOT_DIR."articles",
-    //TODO: Rename this to asset_url.
-    //TODO: This will break once we are online and 
-    //we dont have the localhost/dreamcach.es this should be base url plu
-    'asset_dir' => "assets/",
-    'layout' => 'layout',
-    'featured_article' => 'hello-world',
-    'router' => array(
-        'basePath' => ''
-     ),
-     //TODO: We should we able to get this by default.
-    'base_url' => 'http://goliatone.com/',
-    
-    'analytics_code'=>'UA-XXXXXXX-1',
-    
-    'backend_storage' =>array(
-        'default'=>'dropbox',
-        'dropbox'=>array(
-            'class' => 'goliatone\flatg\backend\drivers\DropboxDriver',
-            'key'=>$passwords['dropbox']['key'],
-            'secret'=>$passwords['dropbox']['secret'],
-            'folder'=>'/articles/'
-            
-        ),
-        'github'=>array(
-            'class' => 'goliatone\flatg\backend\drivers\GithubDriver',
-            'key'=>$passwords['github']['key'],
-            'secret'=>$passwords['github']['secret'],
-            'repo'=>'https://github.com/goliatone/jii',
-            'branch'=>'gh-pages'
-            
-        ),
-    ),
+    'key1'=>'value1_override',
+    'key3'=>array(
+        'key31'=>31,
+        'key4'=>'value4_override'
+    )
 );
