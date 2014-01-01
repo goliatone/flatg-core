86fed7a17a6650858677868ccc6345a3fdcab769
goliatone <burgosemi@gmail.com>
2013-12-31 22:09:51 -0500

    Updated sage runner.

--- sage.php
+++ sage.php
@@ -2,23 +2,24 @@
 
 	include __DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 
-	use TokenReflection;
-
+    use TokenReflection\Broker as TBroker;
+	use TokenReflection\Broker\Backend\Memory as TMemory;
+    
 	if (!isset($argv[1])) {
 		echo 'Usage: php sage.php <input_path> [<output_path>]' . PHP_EOL;
 		exit(1);
 	}
 
-	$backend             = new TokenReflection\Broker\Backend\Memory();
-	$broker              = new TokenReflection\Broker($backend);
+	$backend             = new TMemory();
+	$broker              = new TBroker($backend);
 	$sage                = new Generator($broker);
 	$document_collection = $sage->run($argv[1]);
 	$template_directory  = __DIR__ . DIRECTORY_SEPARATOR . 'vendor/dotink/sage/templates';
-
-	(new Writer(isset($argv[2]) ? $argv[2] : NULL, $template_directory))
-		-> setExternalDocs($sage->getConfig('external_docs'))
-		-> setTemplateData($sage->getConfig('template_data'))
-		-> buildDocumentation($document_collection);
+    $arg1   = isset($argv[2]) ? $argv[2] : NULL; 
+    $writer = new Writer($arg1, $template_directory);
+	$writer->setExternalDocs($sage->getConfig('external_docs'))
+		->setTemplateData($sage->getConfig('template_data'))
+		->buildDocumentation($document_collection);
 
 	exit(0);
 }
