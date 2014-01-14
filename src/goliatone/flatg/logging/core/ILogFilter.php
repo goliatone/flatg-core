6674c5878c13ad38dd5a2442ef9075562eade1b1
goliatone <burgosemi@gmail.com>
2014-01-14 18:50:48 -0500

    Logger: Updated ILogFilter interface.

--- src/goliatone/flatg/logging/core/ILogFilter.php
+++ src/goliatone/flatg/logging/core/ILogFilter.php
@@ -1,4 +1,4 @@
-<?php namespace goliatone\events\core {
+<?php namespace goliatone\flatg\logging\core {
 
     use goliatone\flatg\logging\core\LogMessage;
 
@@ -7,6 +7,12 @@
      * over what `LogMessage` get logged,
      * beyond the control that is provided
      * by the ILogLevel.
+     * They can be applied at two different
+     * moments of the publishing process;
+     * before the message is compiled, and
+     * after the message has been compiled.
+     *
+     *
      *
      * Interface ILogFilter
      * @package goliatone\events\core
@@ -25,6 +31,42 @@
          * @return bool
          */
         public function collect(LogMessage $message);
+
+        /**
+         * Returns `TRUE` if the filter
+         * is to be applied as a pre process.
+         *
+         * Defaults to `FALSE`
+         *
+         * @return bool
+         */
+        public function isPreProcess();
+
+
+        /**
+         * @return ILogFilter
+         */
+        public function getParent();
+
+        /**
+         * @param  LogLevel $level
+         * @return mixed
+         */
+        public function setLevel(LogLevel $level);
+
+        /**
+         * @return ILogLevel
+         */
+        public function getLevel();
+
+
+        /**
+         * Defaults to NULL, thus not
+         * filtering any namespace.
+         *
+         * @return string
+         */
+        public function getNamespace();
 
     }
 }
