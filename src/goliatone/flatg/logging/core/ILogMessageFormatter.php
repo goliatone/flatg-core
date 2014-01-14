1546d034b503782dfad232d124079b230d29d5c3
goliatone <burgosemi@gmail.com>
2014-01-14 18:51:22 -0500

    Logger: ILogMessageFormatter, added PHPDoc strings.

--- src/goliatone/flatg/logging/core/ILogMessageFormatter.php
+++ src/goliatone/flatg/logging/core/ILogMessageFormatter.php
@@ -1,11 +1,24 @@
-<?php namespace goliatone\events\core {
+<?php namespace goliatone\flatg\logging\core {
 
 
     use goliatone\flatg\logging\core\LogMessage;
 
+    /**
+     * A formatter defines the text presentation
+     * of a `LogMessage`.
+     * `ILogPublisher` are associated
+     * with one or more `ILogFormatter`.
+     *
+     * Interface ILogFormatter
+     * @package goliatone\events\core
+     */
     interface ILogMessageFormatter
     {
 
+        /**
+         * @param LogMessage $message
+         * @return LogMessage
+         */
         public function format(LogMessage $message);
     }
 }
