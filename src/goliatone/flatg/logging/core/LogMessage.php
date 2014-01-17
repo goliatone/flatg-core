62b6b388109128332b94e148c0313fd43e28a79f
goliatone <burgosemi@gmail.com>
2014-01-17 18:03:41 -0500

    Logger: LogMessage, docs, refactored.

--- src/goliatone/flatg/logging/core/LogMessage.php
+++ src/goliatone/flatg/logging/core/LogMessage.php
@@ -11,18 +11,25 @@
      *
      * Class LogMessage
      *
+     * Every time we set a property using one of its
+     * setters we set a flag marking the object as dirty
+     * and needing of another rendering- call to updateMessage.
+     *
+     * If we set a property using Array like notation we
+     * do not set the flat, so the change goes unnoticed.
+     *
      * @method setLevel() setLevel(LogLevel $level)
      * @method string getLevel()
-     * @method setLogger() setLogger(string $logger)
+     * @method setLogger() setLogger(\string $logger)
      * @method string getLogger()
-     * @method setMessage() setMessage(string $message)
+     * @method setMessage() setMessage(\string $message)
      * @method string getMessage()
      * @method string getRawMessage()
      * @method setStackTrace() setStackTrace(StackTrace $stackTrace)
      * @method string getStackTrace()
-     * @method string setTimestamp() setTimestamp(DateTime $timestamp)
+     * @method string setTimestamp() setTimestamp(\DateTime $timestamp)
      * @method string getTimestamp()
-     * @method setAddress() setAddress(string $machine)
+     * @method setAddress() setAddress(\string $machine)
      * @method string getAddress()
      *
      * @package goliatone\events\core
@@ -36,8 +43,25 @@
          */
         protected $_rawMessage = '';
 
-
+        /**
+         * When solving the string template and
+         * a token is not found in context,
+         * should we remove it from final output
+         * or remove it?
+         *
+         * @var bool Default FALSE
+         */
         public $consumeUnusedTokens = FALSE;
+
+        /**
+         * Flag indicating if new data has
+         * been added suggesting that the message
+         * might need an update render.
+         *
+         * @var bool
+         */
+        protected $_isDirty = FALSE;
+
 
         /**
          * @var array
@@ -45,10 +69,20 @@
         protected $_context = array();
 
 
+        /**
+         * @param $level
+         * @param $message
+         * @param array $context
+         * @param $name
+         * @param int $stackTraceSkip
+         * @return LogMessage
+         */
         static public function factory($level, $message, array $context = array(), $name, $stackTraceSkip=3)
         {
-            $log = new LogMessage($level, $message, $context);
+            $log = new LogMessage($level, $message, $context, FALSE);
             $log->setLogger($name);
+
+            $log->setTimestamp(new \DateTime('NOW'));
 
             $stackTrace = Debugger::backtrace($stackTraceSkip);
             $log->setStackTrace($stackTrace);
@@ -56,15 +90,18 @@
             $address = Utils::getServerAddress( );
             $log->setAddress($address);
 
+            $log->updateMessage();
+
             return $log;
         }
 
         /**
          * @param LogLevel $level
-         * @param string   $message
-         * @param array    $context
+         * @param string $message
+         * @param array $context
+         * @param bool $autoRender
          */
-        function __construct($level, $message, array $context = array())
+        function __construct($level, $message, array $context = array(), $autoRender = TRUE)
         {
 
             //TODO: We should integrate extras/content with __call and
@@ -81,8 +118,14 @@
 
             $this->setLevel($level);
             $this->setMessage($message);
+            if($autoRender) $this->updateMessage();
         }
 
+        /**
+         * @param $name
+         * @param $arguments
+         * @return mixed
+         */
         function __call($name, $arguments)
         {
             if(preg_match("/^(set|get)/i", $name))
@@ -94,6 +137,7 @@
                     case 'set':
                         $value = $arguments[0];
                         $this->offsetSet($key, $value);
+                        $this->_isDirty = TRUE;
                         break;
                     case 'get':
                         //Access prop stored
@@ -102,17 +146,11 @@
                         //We passed a default value on get{$name}($default)
                         if(count($arguments) > 0)
                             return $arguments[1];
-
-//                        return NULL;
-
-//                        break;
                     default:
                         print('Upsy! The method is not defined, so I don\'t know what to do!');
 
                 }
-
             }
-
         }
 
         /**
@@ -124,11 +162,15 @@
             return $this->getArrayCopy();
         }
 
+        /**
+         *
+         */
         public function updateMessage()
         {
             //TODO, should we implement stringTemplate here and remove dep on Utils?
             $message = Utils::stringTemplate($this->getMessage(), $this->getContext(), $this->consumeUnusedTokens);
             $this->setMessage($message);
+            $this->_isDirty = FALSE;
         }
     }
 }
