e1a8bf2eb5f59242ed1216e38a44486f05d31d73
goliatone <burgosemi@gmail.com>
2014-01-19 18:18:35 -0500

    Logging: AbstractPublisher, implemented sync/async. Added header/footer for publisher. Added stringTemplate method, uses Utils, minimize direct dependency on Utils. Ensure that we get a terminate/begin cycle without having to call directly parent from extending classes. Refactored flush method, takes an array to batch process all events at once.

--- src/goliatone/flatg/logging/publishers/clients/AbstractPublisher.php
+++ src/goliatone/flatg/logging/publishers/clients/AbstractPublisher.php
@@ -5,36 +5,170 @@
     use goliatone\flatg\logging\core\ILogPublisher;
     use goliatone\flatg\logging\core\ILogMessageFormatter;
     use goliatone\flatg\logging\formatters\CompoundFormatter;
+    use goliatone\flatg\logging\helpers\Utils;
 
+    /**
+     * TODO: Add RunTimeAugmenter.
+     *
+     * Class AbstractPublisher
+     * @package goliatone\flatg\logging\publishers\clients
+     */
     abstract class AbstractPublisher implements ILogPublisher
     {
+
+        /**
+         * @var null
+         */
+        public $header = null;
+
+        /**
+         * @var null
+         */
+        public $footer = null;
+
+        /**
+         * @var array
+         */
+        public $messages = array();
+
+        /**
+         * @var bool
+         */
+        public $asynchronous = FALSE;
+
+///////////////////////////////////////
+//      TODO: Move to Debugger::stopWatch?
+///////////////////////////////////////
+        /**
+         * @var null
+         */
+        public $startTime = null;
+
+        /**
+         * @var null
+         */
+        public $endTime   = null;
+
+        /**
+         * @var null
+         */
+        public $runTime   = null;
+///////////////////////////////////////
 
         /**
          * @var CompoundFormatter
          */
         protected $_formatter;
 
+        /**
+         * @var string
+         */
         protected $_defaultFormatterClass;
 
+        /**
+         * @var string
+         */
         protected $_name = __CLASS__;
 
+        /**
+         * @var bool
+         */
+        protected $_runOnConstruct = TRUE;
+
+        /**
+         *
+         */
         function __construct()
         {
             $this->_formatter = new CompoundFormatter();
             $this->_defaultFormatterClass = 'goliatone\flatg\logging\formatters\SimpleFormatter';
+
+            if($this->_runOnConstruct)
+                $this->doBegin();
         }
 
 
         /**
          * @inheritdoc
          */
-        abstract public function publish(LogMessage $message);
+        public function publish(LogMessage $message)
+        {
+            $this->messages[] = $message;
+
+            if($this->asynchronous) return $this;
+
+            $this->onPublish($message);
+
+            return $this;
+        }
+
+        protected function onPublish(LogMessage $message)
+        {
+            //This has to be overwritten
+        }
 
         /**
          * @inheritdoc
          */
-        abstract public function flush(LogMessage $message);
+        public function flush(array $messages = null)
+        {
+            if(!$messages) $messages = array();
+            $load = array_merge($this->messages, $messages);
+            foreach($load as $message)
+            {
+                $this->onPublish($message);
+            }
+        }
 
+        /**
+         * @param $header
+         * @return mixed
+         */
+        public function setHeader($header)
+        {
+            $this->header = $header;
+        }
+
+        /**
+         * @param $footer
+         * @return mixed
+         */
+        public function setFooter($footer)
+        {
+            $this->footer = $footer;
+        }
+
+        /**
+         *
+         */
+        protected function doBegin()
+        {
+            $this->startTime = microtime(true);
+
+            $this->begin();
+        }
+
+        /**
+         *
+         */
+        public function begin()
+        {
+
+        }
+
+        /**
+         *
+         */
+        protected function doTerminate()
+        {
+            if($this->asynchronous) $this->flush();
+
+            $this->endTime = microtime(true);
+            $this->runTime = ($this->endTime - $this->startTime)/60;
+
+            $this->terminate();
+
+        }
         /**
          * @return void
          */
@@ -75,10 +209,20 @@
          */
         public function getDefaultFormatter()
         {
-
             $_DefaultFormatter = $this->_defaultFormatterClass;
 
             return new $_DefaultFormatter();
+        }
+
+        /**
+         * @param $message
+         * @param $context
+         * @param bool $consume
+         * @return string
+         */
+        protected function stringTemplate($message, $context, $consume = TRUE)
+        {
+            return Utils::stringTemplate($message, $context, $consume);
         }
 
         /**
@@ -87,7 +231,7 @@
         public function __destruct()
         {
             try {
-                $this->terminate();
+                $this->doTerminate();
             } catch(\Exception $e) {
                 #Swallow it, just like that?!
             }
