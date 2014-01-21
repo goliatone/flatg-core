56538a7e3ed902385b3af762489b76b8c2c2a0ff
goliatone <burgosemi@gmail.com>
2014-01-21 14:46:07 -0500

    Logging: BaseTransformer, refactored configuration out to CoreObject.

--- src/goliatone/flatg/logging/formatters/transformers/BaseTransformer.php
+++ src/goliatone/flatg/logging/formatters/transformers/BaseTransformer.php
@@ -1,5 +1,6 @@
 <?php namespace goliatone\flatg\logging\formatters\transformers {
 
+    use goliatone\flatg\logging\core\ConfigurableObject;
     use goliatone\flatg\logging\helpers\Utils;
     use goliatone\flatg\logging\core\ILogMessageFormatTransformer;
 
@@ -7,7 +8,7 @@
      * Class BaseTransformer
      * @package goliatone\flatg\logging\formatters\transformers
      */
-    class BaseTransformer implements ILogMessageFormatTransformer
+    class BaseTransformer extends ConfigurableObject implements ILogMessageFormatTransformer
     {
         /**
          * @var array
@@ -31,44 +32,6 @@
         {
             $this->_type = $type;
             $this->_delegated = 'transform';
-        }
-
-        /**
-         * TODO: Move to IConfigurable.
-         * TODO: Move to BaseObject.
-         *
-         * @param array $config
-         * @throws \InvalidArgumentException
-         */
-        public function configure($config=array())
-        {
-            if(is_string($config))
-            {
-                if(!($args = func_get_args()))
-                    throw new \InvalidArgumentException("Configure: If first param is string, second must be array!");
-                $config = array($config=>func_get_args());
-            }
-            //TODO: Move to Utils?
-            $getValue = function($value){
-                if(!$value && $value !== FALSE) return null;
-                return is_callable($value) ? call_user_func_array($value, func_get_args()) : $value;
-            };
-
-            //TODO: Move to Utils?
-            $setValue = function($scope, $name, $value){
-                if(is_callable(array($scope, $name))) return call_user_func(array($scope, $name), $value);
-                if(property_exists($scope, $name))
-                {
-                    if(is_array($scope))       return $scope[$name] = $value;
-                    else if(is_object($scope)) return $scope->$name = $value;
-                }
-            };
-
-            foreach($config as $prop => $value)
-            {
-                $value = $getValue($value);
-                $setValue($this, $prop, $value);
-            }
         }
 
         /**
