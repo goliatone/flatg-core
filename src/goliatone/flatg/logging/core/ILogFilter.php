01a38edc9885fd420b73e5f15b2772031edee005
goliatone <burgosemi@gmail.com>
2014-01-22 11:26:22 -0500

    Logging: ILogFilter, refactoring.

--- src/goliatone/flatg/logging/core/ILogFilter.php
+++ src/goliatone/flatg/logging/core/ILogFilter.php
@@ -65,6 +65,8 @@
     {
 
         /**
+         * TODO: Does this sound right? It should return TRUE if it's filtered and thus not allowed to pass.
+         *
          * Returns TRUE if the filter can
          * be published. If the LogMessage
          * belongs to a `namespace` that is
@@ -76,7 +78,7 @@
          * @param  LogMessage $message
          * @return bool
          */
-        public function collect(LogMessage $message);
+        public function sift(LogMessage $message);
 
         /**
          * Returns `TRUE` if the filter
@@ -92,9 +94,21 @@
         /**
          * @return ILogFilter
          */
-        public function getParent();
+        public function getNext();
 
         /**
+         * @param ILogFilter $filter
+         * @return mixed
+         */
+        public function addNext(ILogFilter $filter);
+
+        /**
+         * @return mixed
+         */
+        public function clear();
+
+        /**
+         *
          * @param  LogLevel $level
          * @return mixed
          */
@@ -105,6 +119,44 @@
          */
         public function getLevel();
 
+
+        /**
+         * @param $rate
+         * @return $this
+         */
+//        public function setRate($rate);
+
+        /**
+         * LogLevel of events to be filtered. Anything
+         * not equal- strict- or at/below should be filtered
+         * if maxBurst has been exceded.
+         * @param $burst
+         * @return $thisÅ
+         */
+//        public function setBurst($burst);
+
+        /**
+         * What should we return on match?
+         * `$action` should be one of the following
+         * `ACCEPT`, `NEUTRAL`, `DENY`.
+         * We need to set both, onMatch and onMisMatch
+         * since we have a trinary state rather than a
+         * binary.
+         *
+         * @param  int $action
+         * @return int
+         */
+        public function onMatchShould($action);
+
+        /**
+         * What should we return on match?
+         * `$action` should be one of the following
+         * `ACCEPT`, `NEUTRAL`, `DENY`.
+         *
+         * @param  int $action
+         * @return int
+         */
+        public function onMisMatchShould($action);
 
         /**
          * Defaults to NULL, thus not
