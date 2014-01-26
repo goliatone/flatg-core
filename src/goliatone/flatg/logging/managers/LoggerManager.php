7dd249c11a715c64e0d1328dfd4cb82bf5f08250
goliatone <burgosemi@gmail.com>
2014-01-25 23:33:43 -0500

    Logging: First implementation of LoggerManger using IOC.

--- src/goliatone/flatg/logging/managers/LoggerManager.php
+++ src/goliatone/flatg/logging/managers/LoggerManager.php
@@ -1,11 +1,20 @@
 <?php namespace goliatone\flatg\logging\managers {
 
+    use goliatone\flatg\di\IOC;
     use goliatone\flatg\logging\helpers\CachedHash;
     use goliatone\flatg\logging\helpers\Utils;
-    use goliatone\flatg\logging\loggers\DefaultLogger;
 
     /**
-     * Class LoggerManager
+     * Class LoggerManager takes care of building
+     * logger instances.
+     * It will store a cache for previously built
+     * loggers, and if we ask multiple times for a logger
+     * with the same id it will return the same instance.
+     *
+     * The LoggerManager also takes care of configuring the
+     * object. It will create all necessary filters, publishers
+     * and configure those as well.
+     *
      * @package goliatone\flatg\logging\managers
      */
     class LoggerManager
@@ -15,10 +24,49 @@
          */
         protected $_cache;
 
+        /**
+         * @var \goliatone\flatg\di\IOC
+         */
+        protected $ioc;
+
+        /**
+         * @var array
+         */
         protected $_defaults = array(
-            'loggerClass'=>'goliatone\\flatg\\logging\\loggers\\DefaultLogger',
-            'formatter'  =>'goliatone\\flatg\\logging\\formatters\\SimpleFormatter',
-            'publisher'  =>'goliatone\\flatg\\logging\\publishers\\clients\\TerminalPublisher',
+            'logger' =>array(
+                'factory' => "@logging.loggers.DefaultLogger",
+                'options' => array(
+                    'methods' => array(
+                        'addPublisher' => '@TerminalPublisher'
+                    )
+                )
+            ),
+            'NullLogger' => array(
+                'factory' => '@logging.loggers.NullLogger'
+            ),
+            'TerminalPublisher'=>array(
+                'factory' => "@logging.publishers.clients.TerminalPublisher",
+                'options' => array(
+                    'methods'=> array(
+                        'addFormatter' => '@SimpleFormatter',
+//                            'addFilter'=>'FilterStringMatch'
+                    )
+                )
+            ),
+            'SimpleFormatter'=>array(
+                'factory' => "@logging.formatters.SimpleFormatter",
+            ),
+            'LogFilter' => array(
+                'factory' => "@logging.filters.LogFilter",
+            )
+            /*,'FilterStringMatch'=>array(
+                'factory' => "@logging.filters.FilterStringMatch",
+                'options'=>array(
+                    'arguments'=>array(
+                        'here'
+                    )
+                )
+            )*/
         );
         /**
          *
@@ -26,11 +74,16 @@
         public function __construct()
         {
             $this->_cache = new CachedHash();
+            $this->ioc = new IOC();
+
         }
+
 
         /**
          * @param $forItem
          * @param array $options
+         *
+         * @return ILogger
          */
         public function buildLogger($forItem, $options = array())
         {
@@ -39,35 +92,36 @@
             return $this->_cache->get($key, array($this, 'build'), $forItem, $options);
         }
 
+        /**
+         * @param $forItem
+         * @param array $options
+         * @return ILogger
+         */
         public function build($forItem, $options=array())
         {
+            //Merge config with defaults.
             $config = array_merge_recursive($this->_defaults, $options);
 
-            $_LoggerClass = $config['loggerClass'];
+            //We want to build a logger. We get the package for the given
+            //object. Then we check to see if there is a config item with
+            //what key, if not we build for a default key: logger.
+            $package   = Utils::qualifiedClassName($forItem);
+            $reference = array_key_exists($package, $config) ? $package : 'logger';
 
-            //This should be the default
-            $_SimpleFormatter   = $config['formatter'];
-            //A publisher should have a related formatter, we should config that
-            $_TerminalPublisher = $config['publisher'];
+            $aliases = array(
+                '@logging' =>  'goliatone.flatg.logging'
+            );
 
-            $formatter = new $_SimpleFormatter();
-            $publisher = new $_TerminalPublisher();
-            $publisher->addFormatter($formatter->getName(), $formatter);
+            $this->ioc->registerAlias($aliases);
+            $this->ioc->configure($config);
 
+            //Solve should run all solvers and return our final object.
+            $logger = $this->ioc->build($reference);
 
-            //How do we get logger class? Default or from config. We should merge.
-            $logger = new $_LoggerClass($forItem);
-
-            //we should do logger < publisher < formatter
-            //TODO: Logger should have no clue about FORMATTERS.
-            $logger->addPublisher($publisher->getName(), $publisher);
-            $logger->addFormatter($publisher->getName(), $formatter);
-
-            $logger->configure($options);
-
+            //we should configure logger.
+            
             return $logger;
         }
-
 
         /**
          * @param $forItem
